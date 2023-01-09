63b8dd433a911bed19ff0f0d97b5b73960a30bfc
SzewczykMichal <13821@eppuz.edu.pl>
2023-01-10 00:36:51 +0100

    poprawiono nav (dodano zakladkę home, zmieniono działanie), wygląd strony, dodano tytuł

--- Script/index.js
+++ Script/index.js
@@ -1,18 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var numerslajdu = 0;
+    window.onscroll = function() { myFunction() };
+    var nav = document.getElementById("nav");
+    var sticky = nav.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            nav.classList.add("sticky")
+        } else {
+            nav.classList.remove("sticky");
+        }
+    }
+    var numerslideu = 0;
     slider();
 
     function slider() {
         var i;
-        var slajd = document.getElementsByClassName("slajd");
-        for (i = 0; i < slajd.length; i++) {
-            slajd[i].style.display = "none";
+        var slide = document.getElementsByClassName("slide");
+        for (i = 0; i < slide.length; i++) {
+            slide[i].style.display = "none";
 
         }
-        numerslajdu++;
-        if (numerslajdu > slajd.length) { numerslajdu = 1 }
-        slajd[numerslajdu - 1].style.display = "block";
+        numerslideu++;
+        if (numerslideu > slide.length) { numerslideu = 1 }
+        slide[numerslideu - 1].style.display = "block";
         setTimeout(slider, 4000);
     }
 });
