ac0dc51c6248885eeb642683504c14f11cfd3935
SzewczykMichal <13821@eppuz.edu.pl>
2023-01-10 12:24:05 +0100

    Poprawa wyglądu, dodanie czcionki etc

--- Script/index.js
+++ Script/index.js
@@ -1,16 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    window.onscroll = function() { myFunction() };
-    var nav = document.getElementById("nav");
-    var sticky = nav.offsetTop;
-
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            nav.classList.add("sticky")
-        } else {
-            nav.classList.remove("sticky");
-        }
-    }
     var numerslideu = 0;
     slider();
 
